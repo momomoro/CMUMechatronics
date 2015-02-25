abc5746fcc3beda9038deccf122c82315b905e85
unknown <wull2think@gmail.com>
2015-02-25 15:57:33 -0500

    Added all code used for motors lab and system demos 1 & 2

--- cameraTest/cameraTest.ino
+++ cameraTest/cameraTest.ino
@@ -12,6 +12,20 @@
 #include <Pixy.h>
 
 Pixy pixy;
+const int numReadings = 10;
+const int LEFT_THRESHOLD = 36;
+const int RIGHT_THRESHOLD = 38;
+
+int greenAverage = 0;
+int greenReadings[numReadings];
+int greenReadIndex = 0;
+int greenTotal = 0;
+
+int yellowAverage = 0;
+int yellowReadings[numReadings];      
+int yellowReadIndex = 0;
+int yellowTotal = 0;
+
 
 void setup()
 {
@@ -23,11 +37,13 @@ void setup()
 }
 
 void loop()
-{ 
+{
   static int i = 0;
   int j;
   uint16_t blocks;
   char buf[32]; 
+  int greenIndex = 0;
+  int yellowIndex = 0;
   
   blocks = pixy.getBlocks();
   
@@ -44,16 +60,85 @@ void loop()
         sprintf(buf, "  block %d: ", j);
         Serial.print(buf); 
         pixy.blocks[j].print();
-        if(pixy.blocks[j].signature == 1) 
+        if(pixy.blocks[j].signature == 2) 
         {
           //turnMotorRight
+          greenIndex = j;
         }
-        else if(pixy.blocks[j].signature == 2)
+        else if(pixy.blocks[j].signature == 1)
         {
           //turnMotorLeft
+          yellowIndex = j;
         }
       }
+      int greenHeight = greenSmooth(pixy.blocks[greenIndex].height);
+      int yellowHeight = yellowSmooth(pixy.blocks[yellowIndex].height);
+        Serial.println(greenHeight);
+        Serial.println(yellowHeight);
+      if(greenHeight > yellowHeight){
+        Serial.println("Corner on the right");
+        if(greenHeight < RIGHT_THRESHOLD) {
+          Serial.println("   Sharp");
+        }
+        else {
+          Serial.println("   Round");
+        }
+      }
+      else {
+        Serial.println("Corner on the left");
+        if(yellowHeight < LEFT_THRESHOLD) {
+          Serial.println("   Sharp");
+        }
+        else {
+          Serial.println("   Round");
+        }
+      }
+      
     }
   }  
+}
+
+int greenSmooth(int height) {
+  // subtract the last reading:
+  greenTotal= greenTotal - greenReadings[greenReadIndex];         
+  // read from the sensor:  
+  greenReadings[greenReadIndex] = height;
+  // add the reading to the total:
+  greenTotal= greenTotal + greenReadings[greenReadIndex];       
+  // advance to the next position in the array:  
+  greenReadIndex = greenReadIndex + 1;                    
+
+  // if we're at the end of the array...
+  if (greenReadIndex >= numReadings)              
+    // ...wrap around to the beginning: 
+    greenReadIndex = 0;                           
+
+  // calculate the average:
+  greenAverage = greenTotal / numReadings;         
+  // send it to the computer as ASCII digits
+  //Serial.println(average);   
+  return greenAverage; 
+}
+
+int yellowSmooth(int height) {
+  // subtract the last reading:
+  yellowTotal= yellowTotal - yellowReadings[yellowReadIndex];         
+  // read from the sensor:  
+  yellowReadings[yellowReadIndex] = height;
+  // add the reading to the total:
+  yellowTotal= yellowTotal + yellowReadings[yellowReadIndex];       
+  // advance to the next position in the array:  
+  yellowReadIndex = yellowReadIndex + 1;                    
+
+  // if we're at the end of the array...
+  if (yellowReadIndex >= numReadings)              
+    // ...wrap around to the beginning: 
+    yellowReadIndex = 0;                           
+
+  // calculate the average:
+  yellowAverage = yellowTotal / numReadings;         
+  // send it to the computer as ASCII digits
+  //Serial.println(average);   
+  return yellowAverage; 
 }
 
