0f86b845f4f2e1116204d2dbc8a3b82190b33a55
Will Moro <wmilner@andrew.cmu.edu>
2015-03-03 11:25:24 -0800

    Tray and Arm movement code

---
+++ tray_move/tray_move.ino
@@ -0,0 +1,37 @@
+
+// Copyright (C) 2012 Mike McCauley
+// $Id: Random.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
+
+#include <AccelStepper.h>
+
+// Define a stepper and the pins it will use
+AccelStepper stepper; // Defaults to AccelStepper::FULL4WIRE (4 pins) on 2, 3, 4, 5
+AccelStepper stepper2(AccelStepper::FULL4WIRE, 6, 7, 8, 9);
+
+void setup()
+{  
+  stepper.setMaxSpeed(50);
+  stepper.setAcceleration(20);
+  stepper2.setMaxSpeed(50);
+  stepper2.setAcceleration(20);
+}
+
+void loop()
+{  //Tray movement
+  for(int i = 0; i < 4; i++) { 
+    //Arm movement
+    for(int j = 0; j < 5; j++) {
+      //part acknowledged 
+      if (gateSensor) {
+        stepper.moveTo(j*5 + 5); //need to play with this number
+      }
+      stepper.run();
+      //move arm back to home
+      stepper.moveTo(0);
+      stepper.run();
+    }
+    //move tray
+    stepper2.move(10);
+    stepper.run()
+  }
+}
